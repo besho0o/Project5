d588e1e7e034c605bf5aa8b368fd0c11aaec0ad6
avocado-toast99 <37838166+avocado-toast99@users.noreply.github.com>
2018-10-27 19:23:33 +0300

    Add files via upload

---
+++ Kruskal.java
@@ -0,0 +1,22 @@
+/*
+ Group Members:
+ Algorithms and Data Structures
+ Section CH 
+ Project 5 - Kruskal's Algorithm 
+ */
+package kruskal;
+
+/**
+ *
+ * @author maha
+ */
+public class Kruskal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
