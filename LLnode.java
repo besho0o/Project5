753a0caa6504c7059964787128e24e595392cdf1
avocado-toast99 <37838166+avocado-toast99@users.noreply.github.com>
2018-11-06 14:43:44 +0300

    Add files via upload

---
+++ LLnode.java
@@ -0,0 +1,96 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kruskal;
+
+/**
+ *
+ * @author maha
+ */
+public class LLnode {
+    // declare 
+    private int index;
+    private char label;
+    private int weight;
+    private LLnode next;
+    private int order;
+    private boolean checked = false;
+    
+    
+   // create constructors:
+    // empty constructor:
+    public LLnode() {
+        // initialize
+        index = 0;
+        weight=-1;
+        next = null;
+        checked=false;
+    }
+    
+    
+    // create constructor for weighted nodes
+      public LLnode(int index, char label, LLnode next) {
+        // initialize
+        this.index = index;
+        this.next = next;
+        this.label=label;
+        checked=false;
+    }
+      
+   
+    // create constructor for weighted nodes
+      public LLnode(int index, char label, int weight, LLnode next) {
+        // initialize
+        this.index = index;
+        this.weight= weight;
+        this.next = next;
+        this.label=label;
+        checked=false;
+    }
+    // getters + setters:
+    public int getIndex() {
+        return index;
+    }
+
+    public LLnode getNext() {
+        return next;
+    }
+
+    public void setIndex(int index) {
+        this.index = index;
+    }
+
+    public void setNext(LLnode next) {
+        this.next = next;
+    }
+    
+     public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public char getLabel() {
+        return label;
+    }
+
+    public void setLabel(char label) {
+        this.label = label;
+    }
+
+    public boolean isChecked() {
+        return checked;
+    }
+
+    public void setChecked(boolean checked) {
+        this.checked = checked;
+    }
+    
+    
+    
+    
+}// class 
---
+++ LinkedList.java
@@ -0,0 +1,176 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kruskal;
+
+/**
+ *
+ * @author Lenovo-Hanaa
+ */
+public class LinkedList {
+
+    // declare
+    private LLnode head;
+    int neighbor;
+    int length;
+
+    // create constructor
+    public LinkedList() {
+
+        // initialize
+        this.head = null;
+        this.length = 0;
+    }
+
+    // getters + setters
+    public LLnode getHead() {
+        return head;
+    }
+
+    public void setHead(LLnode head) {
+        this.head = head;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+    public LLnode getNeighborNot(char x) {
+        LLnode hlpptr = this.head;
+
+        while (hlpptr != null&& !hlpptr.isChecked()) {
+
+            if (hlpptr.getLabel() == x) {
+
+                hlpptr.setChecked(true);
+                hlpptr.getNext();
+            } else {
+                return hlpptr;
+            }
+
+        }
+        return null;
+
+    }
+
+    // this method inserts a node and its weight to the list (weighted graph) 
+    public void InsertNeighbor(int index, char label) {
+        head = InsertNeighbor(head, index, label);
+    }
+
+    private LLnode InsertNeighbor(LLnode head, int index, char label) {
+
+        // if the list is empty OR if the node value is less than the head
+        // we insert in the beginning
+        if (head == null ) {
+            head = new LLnode(index, label, head);
+            return head;
+        } else {
+            // create a help pointer to traverse down the list
+
+            LLnode helpPtr = head;
+
+            while (helpPtr.getNext() != null) {
+                // if the value is less than the node's value, 
+                // then exit the while loop because we will insert at the end 
+                
+                helpPtr = helpPtr.getNext();
+            }
+            // create new node and insert it at the end of the list
+            LLnode newNode = new LLnode(index, label, helpPtr.getNext());
+            helpPtr.setNext(newNode);
+        }
+        // Return head
+        return head;
+    }
+
+//    // this method inserts a node and its weight to the list (weighted graph) 
+//    public void Insert(int index, char label, int weight) {
+//        head = Insert(head, index, label, weight);
+//    }
+//
+//    private LLnode Insert(LLnode head, int index, char label, int weight) {
+//
+//        // if the list is empty OR if the node value is less than the head
+//        // we insert in the beginning
+//        if (head == null || head.getIndex() > index) {
+//            head = new LLnode(index, label, weight, head);
+//            return head;
+//        } else {
+//            // create a help pointer to traverse down the list
+//
+//            LLnode helpPtr = head;
+//
+//            while (helpPtr.getNext() != null) {
+//                // if the value is less than the node's value, 
+//                // then exit the while loop because we will insert at the end 
+//                if (helpPtr.getNext().getIndex() > index) {
+//                    break;
+//                }
+//                helpPtr = helpPtr.getNext();
+//            }
+//            // create new node and insert it at the end of the list
+//            LLnode newNode = new LLnode(index, label, weight, helpPtr.getNext());
+//            helpPtr.setNext(newNode);
+//        }
+//        // Return head
+//        return head;
+//    }
+    // this method prints the list
+    public void PrintNeighbors() {
+        PrintNeighbors(head);
+    }
+
+    private void PrintNeighbors(LLnode head) {
+
+        LLnode helpPtr = head;
+
+        while (helpPtr != null) {
+            // print the index of the helpPtr
+            System.out.print(helpPtr.getLabel() + " ");
+            // Step one node over
+            helpPtr = helpPtr.getNext();
+        }
+        System.out.println("");
+
+        
+        
+    }
+     
+    
+    
+    
+    public  boolean findCycle(LLnode parent, LLnode destination, LinkedList pList, LinkedList dList) {
+
+      
+        if (parent == null || destination == null) {
+            return false;
+
+
+        }
+        if (parent == destination) {
+            return true;
+
+
+        }
+        if (parent == destination) {
+            return true;
+
+
+        }
+        if (pList.getNeighborNot(destination.getLabel()) == dList.getNeighborNot(parent.getLabel())) {
+            return true ;
+
+
+        } else {
+            return findCycle(parent.getNext(), destination.getNext(), pList, dList);
+        }
+    }
+
+}// class
